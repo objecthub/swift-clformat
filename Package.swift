fa7491307cfbffc23dffcacedb6f5a08d1f265cd
Matthias Zenger <matthias@objecthub.net>
2023-05-30 00:27:35 +0200

    Fix platform constraints in Package.swift.

--- Package.swift
+++ Package.swift
@@ -24,6 +24,11 @@ import PackageDescription
 let package = Package(
   name: "CLFormat",
   
+  platforms: [
+    .macOS(.v11),
+    .iOS(.v14)
+  ],
+  
   // Products define the executables and libraries produced by a package, and make them visible
   // to other packages.
   products: [
